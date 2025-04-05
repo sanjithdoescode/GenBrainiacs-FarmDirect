22315eb4ea034893d0b27547e2ef230be82d379b
san <sanjithkarthik16@gmail.com>
2025-04-05 10:25:09 +0530

    Delete app directory

--- app/layout.jsx
+++
@@ -1,4 +0,0 @@
-// We need to update the package.json file to include framer-motion, but since
-// we can't edit it directly, we'll just keep this comment as a reminder that
-// framer-motion needs to be installed with:
-// npm install framer-motion 
